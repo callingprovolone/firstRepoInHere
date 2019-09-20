1b5869de8360a1aa3179cf6df64ae65bc84ac5a5
laptopcerveza <joseperezprogramming@outlook.com>
2019-09-20 16:07:16 -0700

    New components added using reactstrap, will submit more components

--- react-visa-page/src/App.js
+++ react-visa-page/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import ReactStrap from 'reactstrap';
 import './App.css';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+        <Footer />
+        <Navbar />
+      </div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    );
+  }
 }
-
 export default App;
